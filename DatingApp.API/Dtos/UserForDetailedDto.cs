3daaf14dbe072e84f9cb366e2c04fe0dffa8bd0d
Rodrigo Moreira <rodrigomoreira07@gmail.com>
2019-02-22 23:28:48 -0300

    End of section 9

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Dtos
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string PhotoUrl { get; set; }
         public ICollection<PhotosForReturn> Photos { get; set; }
 
     }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -17,6 +17,9 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CanculateAge());
                 });
             CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
                 .ForMember(dest => dest.Age, opt => {
                     opt.ResolveUsing(d => d.DateOfBirth.CanculateAge());
                 });
